457cc40d25fd974befd4178132c0e9520d161709
giscafer <giscafer@outlook.com>
2017-06-12 01:17:07 +0800

    add tslint.json

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -17,7 +17,8 @@ export class CardsPage {
         },
         date: 'November 5, 1955',
         image: 'assets/img/advance-card-bttf.png',
-        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine...' +
+        ' out of a DeLorean?! Whoa. This is heavy.',
       },
       {
         user: {
@@ -26,7 +27,8 @@ export class CardsPage {
         },
         date: 'May 12, 1984',
         image: 'assets/img/advance-card-tmntr.jpg',
-        content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator, can learn the value of human life, maybe we can too.'
+        content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator,' +
+        ' can learn the value of human life, maybe we can too.'
       },
       {
         user: {
